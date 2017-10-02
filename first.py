ac458a4c75112123cf5de864a141598396e29100
BereczkiNorbertCristian <bnorbertcristian@gmail.com>
2017-10-02 11:17:54 +0300

    Added First chapter exercises

--- first.py
+++ first.py
@@ -1,6 +1,37 @@
 import survey
-table = survey.Pregnancies()
-table.ReadRecords()
-for i in range(0,100):
-	print table.records[0].outcome
-print 'Number of pregnancies', len(table.records)
+
+
+def getPregnancyLengths(lst):
+	return [el.prglength for el in lst]
+
+def mean(lst):
+	return float(sum(getPregnancyLengths(lst)))/len(lst) 
+
+def main():
+
+	table = survey.Pregnancies()
+	table.ReadRecords()
+	print 'Number of pregnancies', len(table.records)
+	ans = 0
+	for preg in table.records:
+		if preg.outcome == 1:
+			ans+=1
+	print 'Number of live pregnancies', ans	
+
+	firsts,others = [],[]
+
+	for preg in table.records:
+		if preg.outcome != 1:
+			continue
+		if preg.birthord == 1:
+			firsts.append(preg)
+		else:
+			others.append(preg)
+	
+	print 'Firsts Len', len(firsts)
+	print 'Others Len', len(others)
+
+	print 'Firsts Mean', mean(firsts)
+	print 'Others Mean', mean(others)
+
+main()
